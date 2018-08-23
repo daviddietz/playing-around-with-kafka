8e25bf67bf82502ea9d710d24e42a646d28c264e
David Dietz <daviddietz@Davids-MacBook-Pro.local>
2018-08-23 09:53:37 -0500

    simple producer and consumer

---
+++ src/main/java/KafkaConsumerTest.java
@@ -0,0 +1,36 @@
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+import java.util.Arrays;
+import java.util.Properties;
+
+public class KafkaConsumerTest {
+    public static void main(String[] args) {
+        Properties properties = new Properties();
+
+        // kafka boostrap server
+        properties.setProperty("bootstrap.servers", "127.0.0.1:9092");
+        properties.setProperty("key.deserializer", StringDeserializer.class.getName());
+        properties.setProperty("value.deserializer", StringDeserializer.class.getName());
+        properties.setProperty("group.id", "test");
+//        properties.setProperty("enable.auto.commit", "true"); //true by default
+        properties.setProperty("auto.commit.intervals.ms", "1000");
+        properties.setProperty("auto.offset.reset", "earliest");
+
+        KafkaConsumer<String, String> kafkaConsumer = new KafkaConsumer<String, String>(properties);
+        kafkaConsumer.subscribe(Arrays.asList("second_topic"));
+
+        while(true) {
+            ConsumerRecords<String, String> consumerRecords = kafkaConsumer.poll(100);
+            for(ConsumerRecord<String, String> consumerRecord : consumerRecords) {
+                System.out.println("Partition: " + consumerRecord.partition() + ", " +
+                        "Offset: " + consumerRecord.offset() + ", " +
+                        "Key: " + consumerRecord.key() + ", " +
+                        "Value: " + consumerRecord.value());
+            }
+            kafkaConsumer.commitAsync(); //used to control offset commits
+        }
+    }
+}
---
+++ src/main/java/KafkaProducerTest.java
@@ -0,0 +1,31 @@
+import java.util.Properties;
+
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+public class KafkaProducerTest {
+    public static void main(String[] args) {
+        Properties properties = new Properties();
+
+        // kafka boostrap server
+        properties.setProperty("bootstrap.servers", "127.0.0.1:9092");
+        properties.setProperty("key.serializer", StringSerializer.class.getName());
+        properties.setProperty("value.serializer", StringSerializer.class.getName());
+
+        // producer acks
+        properties.setProperty("acks", "1");
+        properties.setProperty("retries", "3");
+        properties.setProperty("linger.ms", "1");
+
+        Producer<String, String> producer = new org.apache.kafka.clients.producer.KafkaProducer<String, String>(properties);
+        for (int key = 0; key < 10; key++) {
+            ProducerRecord<String, String> producerRecord = new ProducerRecord<String, String>("second_topic", Integer.toString(key),
+                    "message with key: " + Integer.toString(key));
+            producer.send(producerRecord);
+
+        }
+
+        producer.close();
+    }
+}
